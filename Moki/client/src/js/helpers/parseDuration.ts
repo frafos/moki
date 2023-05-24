8b79094ebffc35a658a79df4fa3a7e328ac320ab
Riwan <coefficriwan@gmail.com>
2023-05-24 11:16:24 +0200

    fix: undefined duration display issue

--- Moki/client/src/js/helpers/parseDuration.ts
+++ Moki/client/src/js/helpers/parseDuration.ts
@@ -5,6 +5,7 @@ const MN = 60;
 function parseDuration(duration: number) {
   let secDuration = Math.floor(duration);
   const minutes = Math.floor(secDuration / MN);
+  if (isNaN(minutes)) return "0 min";
   const formattedMin = minutes < 10 ? minutes : format(".2s")(minutes);
   return `${formattedMin} min`;
 }
